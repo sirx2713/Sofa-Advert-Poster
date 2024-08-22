00afc32aa8cc4364d74d99e69b3adc480c074ce5
iSAD <bradx2713@gmail.com>
2024-08-22 08:52:46 +0200

    Initial Commit

---
+++ Sofa Advert Poster/Sofa_Advert_PosterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sofa_Advert_PosterApp.swift
+//  Sofa Advert Poster
+//
+//  Created by Tafadzwa Alexander Razaro on 2024/08/22.
+//
+
+import SwiftUI
+
+@main
+struct Sofa_Advert_PosterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
